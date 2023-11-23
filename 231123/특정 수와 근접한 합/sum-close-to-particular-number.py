04881287dd7f265553720c7728e5008fe23035d1
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-11-23 19:54:40 +0900

    Title: 특정 수와 근접한 합, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 231123/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -4,13 +4,13 @@ numArr = list(map(int, input().split()))
 minDiff = float('inf')
 
 for skipIdx1 in range(len(numArr)):
-    for skipIdx2 in range(len(numArr)):
+    for skipIdx2 in range(skipIdx1, len(numArr)):
         tempSum = 0
 
         for idx in range(len(numArr)):
             if idx != skipIdx1 and idx != skipIdx2:
                 tempSum += numArr[idx]
-        
+                
         minDiff = min(abs(tempSum - target), minDiff)
 
 print(minDiff)
