59e122577031f9328b3e0ba32f7660451df484d9
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-12-13 17:58:21 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 226ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 231213/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const inputAll = () => fs.readFileSync('/dev/stdin').toString().split('\n');
+const print = (...args) => console.log(...args);
+
+const calcDistance = (p1, p2) => {
+    const [r1, c1] = p1;
+    const [r2, c2] = p2;
+
+    return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+}
+
+let [N, ...arr] = inputAll()
+
+N = Number(N)
+arr = arr.map(line => line.split(' ').map(elm => Number(elm)))
+
+let minDis = Infinity;
+
+for(let skip = 1; skip < arr.length - 1; skip++){
+    let tempDis = 0;
+    let prevIdx = 0;
+
+    for(let i = 1; i < arr.length; i++){
+        if(i === skip) continue;
+
+        tempDis += calcDistance(arr[prevIdx], arr[i]);
+        prevIdx = i;
+    }
+
+    minDis = Math.min(minDis, tempDis);
+}
+
+print(minDis)
