07a585824ea3b89b50236f8bc92e1c3b92fb5799
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-12-13 20:44:31 +0900

    Title: 오목, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 231213/오목/O-mok.py
+++ 231213/오목/O-mok.py
@@ -1,4 +1,4 @@
-# 좌 하 좌하만 보면 된다.
+# 좌 하 좌하 우하만 보면 된다.
 # 0, 0이 1, 1이 된다/
 # 찾으면 시작 좌표, 끝 좌표를 기억해서 /2해서 중간 좌표를 구함.
 BOARD = []
@@ -22,10 +22,19 @@ def solution():
                 color = BOARD[i][j]
 
                 try:
-                    # 대각선 확인
+                    # 좌하 대각선 확인
                     if BOARD[i + 1][j + 1] == color and BOARD[i + 2][j + 2] == color and BOARD[i + 3][j + 3] == color and BOARD[i + 4][j + 4] == color:
                         print(color)
                         print(i + 2 + 1, j + 2 + 1)
+                        return
+                except:
+                    pass
+
+                try:
+                    # 우하 대각선 확인
+                    if BOARD[i - 1][j + 1] == color and BOARD[i - 2][j + 2] == color and BOARD[i - 3][j + 3] == color and BOARD[i - 4][j + 4] == color:
+                        print(color)
+                        print(i - 2 + 1, j + 2 + 1)
                         return
                 except:
                     pass
