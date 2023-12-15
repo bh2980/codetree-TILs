ce4a2f969d3a3c85c106d636911dacd6ea61fe2b
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2023-12-15 22:01:27 +0900

    Title: G or H 3, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231215/G or H 3/G-or-H-3.py
@@ -0,0 +1,29 @@
+N, K = map(int, input().split())
+numList = []
+maxCount = 0
+
+for _ in range(N):
+    idx, alpha = input().split()
+    idx = int(idx)
+
+    maxCount = max(idx, maxCount)
+
+    numList.append((idx - 1, 1 if alpha == 'G' else 2))
+
+arr = [0 for _ in range(maxCount)]
+
+for idx, alpha in numList:
+    arr[idx] = alpha
+
+maxSum = sum(arr[:K + 1])
+tempSum = maxSum
+
+for i in range(K + 1, N):
+    previdx = i - K
+
+    tempSum -= arr[previdx]
+    tempSum += arr[i]
+
+    maxSum = max(maxSum, tempSum)
+
+print(maxSum)
